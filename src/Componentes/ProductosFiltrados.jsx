ba25849bbb2d9a27dd2cb96985e86d5ccaec428c
diegoleise <diegoleise@gmail.com>
2025-08-29 20:29:35 -0300

    Cambio tamaño input

--- src/Componentes/ProductosFiltrados.jsx
+++ src/Componentes/ProductosFiltrados.jsx
@@ -12,12 +12,12 @@ const ProductosFiltrados = () => {
     return (
         <>
             
-            <div style={{gap:'10px', display:'flex', justifyContent:'center',marginTop: '20pxp', }}><input style={{borderRadius:'5px', marginTop:'10px', marginTop:'10px', padding:'5px', flexWrap:'wrap'}}
+            <div style={{gap:'10px', display:'flex', justifyContent:'center',marginTop: '20pxp', }}><input style={{borderRadius:'10px', marginTop:'10px', marginTop:'10px', padding:'5px', flexWrap:'wrap'}}
                 placeholder='Buscar productos...'
                 value={busqueda}
                 onChange={(e) => setBusqueda(e.target.value)} />
-                <button style={{marginTop:'10px'}} onClick={()=>setProducts(filtrado) }>Filtrar</button>
-                   <button style={{marginTop:'10px'}} onClick={()=>setProducts(productosFiltrados) }>Todos los Productos</button>
+                <button style={{marginTop:'10px',borderRadius:'10px'}} onClick={()=>setProducts(filtrado) }>Filtrar</button>
+                   <button style={{marginTop:'10px', borderRadius:'10px'}} onClick={()=>setProducts(productosFiltrados) }>Todos los Productos</button>
                 </div>
             <div style={{ display: 'flex',  justifyContent: 'space-evenly', flexWrap:'wrap' }}>
                 
