fb3289ba66f737a8970b950fe06b72a56454b988
diegoleise <diegoleise@gmail.com>
2025-08-30 13:04:15 -0300

    Arreglo de input

--- src/Componentes/ProductosFiltrados.jsx
+++ src/Componentes/ProductosFiltrados.jsx
@@ -5,29 +5,32 @@ import { Context } from "../Context"
 
 const ProductosFiltrados = () => {
 
-    const {  filtrado,setProducts, products, productosFiltrados, busqueda, setBusqueda } = useContext(Context);
+    const { filtrado, setProducts, products, productosFiltrados, busqueda, setBusqueda } = useContext(Context);
 
-   
-    
+
+
     return (
         <>
-            
-            <div style={{gap:'10px', display:'flex', justifyContent:'center',marginTop: '20pxp', }}><input style={{borderRadius:'10px', marginTop:'10px', marginTop:'10px', padding:'5px', flexWrap:'wrap'}}
+        <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+            <div style={{ textAlign: 'center', gap: '10px', marginTop: '20pxp', width: '50%' }}><input style={{ borderRadius: '10px', marginTop: '10px', marginTop: '10px', padding: '5px', width: '100%', boxSizing: 'border-box' }}
                 placeholder='Buscar productos...'
                 value={busqueda}
                 onChange={(e) => setBusqueda(e.target.value)} />
-                <button style={{marginTop:'10px',borderRadius:'10px'}} onClick={()=>setProducts(filtrado) }>Filtrar</button>
-                   <button style={{marginTop:'10px', borderRadius:'10px'}} onClick={()=>setProducts(productosFiltrados) }>Todos los Productos</button>
-                </div>
-            <div style={{ display: 'flex',  justifyContent: 'space-evenly', flexWrap:'wrap' }}>
-                
-               {
+                <button style={{ marginTop: '10px', borderRadius: '10px' }} onClick={() => setProducts(filtrado)}>Filtrar</button>
+                <button style={{ marginTop: '10px', borderRadius: '10px' }} onClick={() => setProducts(productosFiltrados)}>Todos los Productos</button>
+            </div>
+        </div>
+
+
+            <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap' }}>
+
+                {
                     products.map(product => (
                         <Products key={product.id} product={product} />
                     ))
                 }
             </div>
-            
+
         </>
     )
 }
